6f1e1b4e876174671412dc7c5f1aa0834b249121
128na <simutrans.128na@gmail.com>
2018-03-10 14:55:23 +0900

    検索キーワードを引き継げるように

--- resources/views/parts/search-form.blade.php
+++ resources/views/parts/search-form.blade.php
@@ -1,7 +1,7 @@
 <form method="post" action="{{ route('addon.search') }}">
   {{ csrf_field() }}
   <div class="input-group">
-    <input type="text" class="form-control" name="word" placeholder="{{ __('messages.label.keyword') }}">
+    <input type="text" class="form-control" name="word" placeholder="{{ __('messages.label.keyword') }}" value="{{ $word ?? '' }}">
     <div class="input-group-append">
       <input type="submit" class="btn btn-outline-primary" value="{{ __('messages.action.search') }}">
     </div>
