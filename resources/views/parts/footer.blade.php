cd1c04df0de88d113d57af45b91c231d0d73bcd4
128na <simutrans.128na@gmail.com>
2018-03-12 21:50:39 +0900

    言語一覧が場所をとるのでフッターを簡素化

--- resources/views/parts/footer.blade.php
+++ resources/views/parts/footer.blade.php
@@ -1,9 +1,9 @@
 <nav class="navbar fixed-bottom navbar-light navbar-laravel border-top">
   <div class="container">
     <span>
-      {{ config('app.name') }} <small>(version {{ config('app.version')}})</small> created by <a href="https://twitter.com/128Na" target="_blank">@128Na</a>.
+      created by <a href="https://twitter.com/128Na" target="_blank">@128Na</a>.
        /
-      <a href="https://github.com/128na/simu_addon_uploader" target="_blank">Github</a> Pull requests are always welcome!
+      <a href="https://github.com/128na/simu_addon_uploader" target="_blank">Github</a>
     </span>
     <span class="pull-right">
 @foreach(config('app.laguages') as $lang => $label)
--- resources/views/parts/header.blade.php
+++ resources/views/parts/header.blade.php
@@ -1,7 +1,7 @@
 <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
   <div class="container">
     <a class="navbar-brand" href="{{ url('/') }}">
-      {{ config('app.name') }}
+      {{ config('app.name') }} <small>ver {{ config('app.version')}}</small>
     </a>
     <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
