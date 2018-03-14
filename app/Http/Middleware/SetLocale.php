de44f41c1812b9e371dc1e404779326d5f0d6534
128na <simutrans.128na@gmail.com>
2018-03-14 20:27:49 +0900

    インデント修正

--- app/Http/Middleware/SetLocale.php
+++ app/Http/Middleware/SetLocale.php
@@ -6,21 +6,21 @@ use Closure;
 
 class SetLocale
 {
-    /**
-     * Handle an incoming request.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \Closure  $next
-     * @return mixed
-     */
-    public function handle($request, Closure $next)
-    {
-      $lang = $request->route('lang');
+  /**
+   * Handle an incoming request.
+   *
+   * @param  \Illuminate\Http\Request  $request
+   * @param  \Closure  $next
+   * @return mixed
+   */
+  public function handle($request, Closure $next)
+  {
+    $lang = $request->route('lang');
 
-      if (array_key_exists($lang, config('app.languages'))) {
-        logger('locale:'.$lang);
-        \App::setLocale($lang);
-      }
-      return $next($request);
+    if (array_key_exists($lang, config('app.languages'))) {
+      logger('locale:'.$lang);
+      \App::setLocale($lang);
     }
+    return $next($request);
+  }
 }
